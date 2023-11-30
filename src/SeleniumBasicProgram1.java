c7b8e90af231ac4cba4414d670d065afc64b9146
dhanesh299 <dhanesh299@gmail.com>
2023-11-30 18:29:07 +0530

    to be merged message from dhanesh to master

--- src/SeleniumBasicProgram1.java
+++ src/SeleniumBasicProgram1.java
@@ -14,6 +14,7 @@ public class SeleniumBasicProgram1 {
 		// push from user 2
 		// push from user 1
 		// Hey hi
+		// to be merged message from dhanesh to master
 		
 		// Driver setup --> In the current version of Selenium jar package, all 4 browser's drivers are added by default
 		// System.setProperty("webdriver.chrome.driver.","C:\\My Folder\\7-Learning\\QA\\Dependencies\\Web Drivers\\Chrome\\chromedriver-win64\\chromedriver.exe");

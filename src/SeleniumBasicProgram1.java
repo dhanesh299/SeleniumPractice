7392b4e9dc688b6f9c1f0f4657ec3a97b3a45273
dhanesh299 <dhanesh299@gmail.com>
2024-02-12 11:39:42 +0530

    jenkin browser parameter updated as testBrowser in base.java

--- src/SeleniumBasicProgram1.java
+++ src/SeleniumBasicProgram1.java
@@ -20,7 +20,8 @@ public class SeleniumBasicProgram1 {
 		// message 2 from vanjivel to merge with dhanesh
 		// dhanesh
 		
-		// Driver setup --> In the current version of Selenium jar package, all 4 browser's drivers are added by default
+		// Driver setup --> In the current version of Selenium jar package, all 4 browser's drivers are added by default.
+		// Below 2 lines used respectively with manual jars and maven dependency can be avoided.
 		// System.setProperty("webdriver.chrome.driver.","C:\\My Folder\\7-Learning\\QA\\Dependencies\\Web Drivers\\Chrome\\chromedriver-win64\\chromedriver.exe");
 		// WebDriverManager.chromedriver().setup();
 		
